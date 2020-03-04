ec86498b1b38492ab7baeb103fd72e8fdc31c3f9
justinkhado <justinkhado@gmail.com>
2020-03-04 03:58:29 -0800

    updated backprop

--- NN.py
+++ NN.py
@@ -6,49 +6,71 @@ class NeuralNetwork(object):
     '''
     def __init__(self, layer_sizes):
         '''
-        Initializes the sizes, num_layers, weights, and biases.
-
         sizes: list of sizes of each layer
         num_layers: number of layers
         weights: list of weight matrices
         biases: list of bias vectors
+        Z: list of matrices of affine function values
+        A: list of matrices of activations
         '''
         self.num_layers = len(layer_sizes)
         self.weights = [np.random.randn(size_next, size_curr) * 0.01 for size_curr, size_next in zip(layer_sizes[:-1], layer_sizes[1:])]
         self.biases = [np.zeros((size,1)) for size in layer_sizes[1:]]
-        self.activations = []
+        self.Z = []
+        self.A = []
 
-    def fit(self, X_train, y_train, epochs):
+    def fit(self, X_train, y_train, epochs, learning_rate=0.1):
         '''
         Takes in a training set and a number of epochs to train the neural network
         '''
+        X = X_train.T
         for i in range(epochs):
-            self.activations = [X_train]
-            self.feedforward(X_train)
+            self.feedforward(X)
+            self.backprop(self.A[-1], y_train, learning_rate)
 
-
-    def feedforward(self, X_train):
+    def feedforward(self, X):
         '''
         Takes in a training set, performs a forward propagation over the entire training set, and returns the predicted outputs
 
-        Z: vector of the results of the affine function of all samples
-        activations: matrix of all inputs of all samples where the inputs is the X_train initially and then the previous activations every instance after
+        z: vector of the results of the affine function of all samples
+        activations: matrix of all inputs of all samples where the inputs is the X initially and then the previous activations every instance after
         '''
-        activations = X_train
-        for layer in range(self.num_layers - 1):
-            print(layer)
-            Z = self.affine(activations, layer)
-            activations = self.sigmoid(Z)
-        
-        return activations
+        activations = X
+        self.Z = []
+        self.A = [X]
 
-    def backprop(self, y_pred, y_true):
+        for layer in range(self.num_layers - 1):
+            z = self.affine(activations, layer)
+            self.Z.append(z)
+            activations = self.sigmoid(z)
+            self.A.append(activations)
+        
+    def backprop(self, y_pred, y_true, learning_rate):
         '''
         Takes in the predicted output and the true outputs and performs a backpropagation
         '''
-        d_a = self.d_cost(y_pred, y_true)
-        d_z = self.d_sigmoid()
+        dL_da = self.d_cost(y_pred, y_true)
+        for layer in range(self.num_layers - 2, -1, -1):
+            da_dz = self.d_sigmoid(self.Z[layer])
+            dz_dw = self.dw_affine(self.A[layer])
+            dz_db = self.db_affine()
 
+            for i in self.weights:
+                print(i.shape)
+            print()
+            for i in self.A:
+                print(i.shape)
+            print()
+            print(dL_da.shape, da_dz.shape, dz_db.shape)
+
+            dL_dw = np.dot(dL_da * da_dz, dz_dw.T)
+            dL_db = dL_da * da_dz * dz_db
+
+            print()
+            print(dL_dw.shape)
+
+            self.weights[layer] -= learning_rate*dL_dw
+            self.biases[layer] -= learning_rate*dL_db
 
     def affine(self, inputs, layer):
         '''
@@ -63,23 +85,29 @@ class NeuralNetwork(object):
         '''
         return 1.0 / (1.0 + np.exp(-z))
 
-    def cost(self, y_pred, y_true):
+    def cross_entropy(self, y_pred, y_true):
         '''
-        Takes in the predictions calculated from the X_train and compares them to the true values from the y_test using the cross-entropy loss function
+        Takes in the predictions calculated from the X_train and compares them to the true values from the y_test using cross-entropy
         '''
         return -(y_true*np.log10(y_pred) + (1-y_true)*np.log10(1-y_pred))
 
-    def dw_affine(self, inputs, layer):
+    def cost(self, y_pred, y_true):
+        '''
+        Returns the average cost of all the samples using the cross entropy loss function
+        '''
+        return (1/len(y_pred)) * sum(self.cross_entropy(y_pred, y_true))
+
+    def dw_affine(self, inputs):
         '''
         Returns the partial derivative of the affine function with respect to the weights
         '''
         return inputs
     
-    def db_affine(self, inputs, layer):
+    def db_affine(self):
         '''
         Returns the partial derivative of the affine function with respect to the biases
         '''
-        return 1
+        return np.ones(1).reshape(1,1)
 
     def d_sigmoid(self, z):
         '''
@@ -89,7 +117,7 @@ class NeuralNetwork(object):
 
     def d_cost(self, y_pred, y_true):
         '''
-        Returns the partial derivative of the cost function with respect to the activation function
+        Returns the partial derivative of the average cost function with respect to the activation function
         '''
-        return ((1-y_true)/(1-y_pred)) - (y_true/y_pred)
+        return (1/len(y_pred))*(((1-y_true)/(1-y_pred)) - (y_true/y_pred))
     
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from NN import NeuralNetwork
 import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+
 
 layers = [4,3,3,1]
 nn = NeuralNetwork(layers)
@@ -9,15 +14,16 @@ X_train = np.array([[1.,2.,3.,4.,5.],
                     [4.,5.,1.,3.,2.],
                     [1.,2.,2.,2.,2.]])
 
-z = nn.affine(X_train, 0)
-a = nn.sigmoid(z)
 
-z = nn.affine(a, 1)
-a = nn.sigmoid(z)
+cancer = load_breast_cancer()
+X = cancer['data']
+y = cancer['target']
+length = len(cancer['feature_names'])
 
-z = nn.affine(a, 2)
-a = nn.sigmoid(z)
+nn = NeuralNetwork([length,20,10,5,1])
 
-print(nn.feedforward(X_train))
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+nn.fit(X_train, y_train, epochs=100)
 
 
