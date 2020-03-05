added normalizer class; fixed backprop

NN.py
@@ -14,23 +14,33 @@ class NeuralNetwork(object):
         A: list of matrices of activations
         '''
         self.num_layers = len(layer_sizes)
-        self.weights = [np.random.randn(size_next, size_curr) * 0.01 for size_curr, size_next in zip(layer_sizes[:-1], layer_sizes[1:])]
+        self.weights = [np.random.randn(size_next, size_curr)*0.01 for size_curr, size_next in zip(layer_sizes[:-1], layer_sizes[1:])]
         self.biases = [np.zeros((size,1)) for size in layer_sizes[1:]]
         self.Z = []
         self.A = []
 
-    def fit(self, X_train, y_train, epochs, learning_rate=0.1):
+    def fit(self, X_train, y_train, epochs, learning_rate=0.1, verbose=True):
         '''
         Takes in a training set and a number of epochs to train the neural network
         '''
         X = X_train.T
         for i in range(epochs):
+            
+            if verbose:
+                print(f'Epoch: {i+1}/{epochs}')
             self.feedforward(X)
             self.backprop(self.A[-1], y_train, learning_rate)
 
+    def predict(self, X):
+        '''
+        Returns an array of predictions for each input
+        '''
+        self.feedforward(X.T)
+        return self.A[-1]
+
     def feedforward(self, X):
         '''
-        Takes in a training set, performs a forward propagation over the entire training set, and returns the predicted outputs
+        Takes in a training set and performs a forward propagation over the entire training set
 
         z: vector of the results of the affine function of all samples
         activations: matrix of all inputs of all samples where the inputs is the X initially and then the previous activations every instance after
@@ -49,45 +59,32 @@ class NeuralNetwork(object):
         '''
         Takes in the predicted output and the true outputs and performs a backpropagation
         '''
+        num_samples = len(y_pred[0])
         dL_da = self.d_cost(y_pred, y_true)
-        for layer in range(self.num_layers - 2, -1, -1):
-            da_dz = self.d_sigmoid(self.Z[layer])
-            dz_dw = self.dw_affine(self.A[layer])
-            dz_db = self.db_affine()
-
-            for i in self.weights:
-                print(i.shape)
-            print()
-            for i in self.A:
-                print(i.shape)
-            print()
-            print(dL_da.shape, da_dz.shape, dz_db.shape)
-
-            dL_dw = np.dot(dL_da * da_dz, dz_dw.T)
-            dL_db = dL_da * da_dz * dz_db
-
-            print()
-            print(dL_dw.shape)
+        for layer in reversed(range(self.num_layers - 1)):
+            dL_dz = np.multiply(dL_da, self.d_sigmoid(self.Z[layer]))
+            dL_dw = (1.0/num_samples)*np.dot(dL_dz, self.A[layer].T)
+            dL_db = (1.0/num_samples)*np.sum(dL_dz, axis=1, keepdims=True)
+            dL_da = np.dot(self.weights[layer].T, dL_dz)
 
             self.weights[layer] -= learning_rate*dL_dw
             self.biases[layer] -= learning_rate*dL_db
 
     def affine(self, inputs, layer):
         '''
-        Takes in the current layer a vector of inputs values (either the initial inputs or the activations of the last layer) 
-        and returns the affine function
+        Returns the affine function by calculating the sum of a vector of biases and dot product of a matrix of weights and the inputs   
         '''
         return np.dot(self.weights[layer], inputs) + self.biases[layer]
 
     def sigmoid(self, z):
         '''
-        Takes in the affine function and returns the sigmoid activation
+        Returns the sigmoid activation with the affine function as an input
         '''
         return 1.0 / (1.0 + np.exp(-z))
 
     def cross_entropy(self, y_pred, y_true):
         '''
-        Takes in the predictions calculated from the X_train and compares them to the true values from the y_test using cross-entropy
+        Returns a vector consisting of costs of each sample calculated using the cross entropy loss function
         '''
         return -(y_true*np.log10(y_pred) + (1-y_true)*np.log10(1-y_pred))
 
@@ -95,29 +92,17 @@ class NeuralNetwork(object):
         '''
         Returns the average cost of all the samples using the cross entropy loss function
         '''
-        return (1/len(y_pred)) * sum(self.cross_entropy(y_pred, y_true))
-
-    def dw_affine(self, inputs):
-        '''
-        Returns the partial derivative of the affine function with respect to the weights
-        '''
-        return inputs
-    
-    def db_affine(self):
-        '''
-        Returns the partial derivative of the affine function with respect to the biases
-        '''
-        return np.ones(1).reshape(1,1)
+        num_samples = len(y_pred[0])
+        return (1.0/num_samples) * np.sum(self.cross_entropy(y_pred, y_true))
 
     def d_sigmoid(self, z):
         '''
         Returns the partial derivative of the sigmoid function with respect to the affine function
         '''
-        return (np.exp(-z)) / ((1 + np.exp(-z))**2)
+        return (np.exp(-z)) / ((1.0 + np.exp(-z))**2)
 
     def d_cost(self, y_pred, y_true):
         '''
-        Returns the partial derivative of the average cost function with respect to the activation function
+        Returns the partial derivative of the cross entropy cost function with respect to the activation function
         '''
-        return (1/len(y_pred))*(((1-y_true)/(1-y_pred)) - (y_true/y_pred))
-    
+        return ((1.0-y_true)/(1.0-y_pred)) - (y_true/y_pred)

test.py
@@ -1,18 +1,9 @@
 from NN import NeuralNetwork
+from Normalizer import Normalizer
 import numpy as np
-import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
-
-
-layers = [4,3,3,1]
-nn = NeuralNetwork(layers)
-
-X_train = np.array([[1.,2.,3.,4.,5.],
-                    [2.,3.,4.,1.,2.],
-                    [4.,5.,1.,3.,2.],
-                    [1.,2.,2.,2.,2.]])
 
 
 cancer = load_breast_cancer()
@@ -20,10 +11,21 @@ X = cancer['data']
 y = cancer['target']
 length = len(cancer['feature_names'])
 
-nn = NeuralNetwork([length,20,10,5,1])
+nn = NeuralNetwork([length,30,20,10,5,1])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-nn.fit(X_train, y_train, epochs=100)
+normalize = Normalizer()
+normalize.fit(X_train)
+X_train = normalize.transform(X_train)
+X_test = normalize.transform(X_test)
+
+nn.fit(X_train, y_train, epochs=1000, verbose=False)
+predictions = nn.predict(X_test)
+print(nn.cost(predictions, y_test))
+
+
+
+
 
 